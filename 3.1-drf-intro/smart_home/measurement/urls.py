6c67a003d5d19f4e7a5bdc207de01288c48c3711
Aleksey Sidorenko <sidorenko66@gmail.com>
2022-11-24 22:45:20 +0500

    Task 3.1

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import SensorsView, SensorView, MeasurementsView
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsView.as_view()),
+    path('sensors/<pk>/', SensorView.as_view()),
+    path('measurements/', MeasurementsView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,36 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorSerializer, MeasurementSerializer, SensorsSerializer
+
+
+class SensorsView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorsSerializer
+
+    def post(self, request):
+        Sensor(**request.data).save()
+        return Response({'status': 'OK'})
+
+
+class SensorView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def patch(self, request, pk):
+        Sensor.objects.filter(id=pk).update(**request.data)
+        return Response({'status': 'OK'})
+
+
+class MeasurementsView(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        data = request.data
+        if 'sensor_id' not in data and 'sensor' in data:
+            data['sensor_id'] = data['sensor']
+            del data['sensor']
+        Measurement(**data).save()
+        return Response({'status': 'OK'})
